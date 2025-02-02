6c0acbd2543433e44f3946f3b8fe0a621fccf79c
Luke Farrell <lukefarrell2012@gmail.com>
2025-02-02 16:57:42 +0000

    typo correction

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 const isAuthenticated = (req, res, next) => {
   res.header('Access-Control-Allow-Origin', req.headers.origin);
   res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
   try {
