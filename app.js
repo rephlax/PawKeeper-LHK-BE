b838f48a4052072eb79b2a2c68548d7aeb499e32
Kateryna Salata <katerynasalata@icloud.com>
2025-02-08 07:46:46 +0100

    add multiply languages

--- app.js
+++ app.js
@@ -12,30 +12,51 @@ const app = express();
 //   'http://localhost:5005'
 // ];
 
+// i18next setup
+const i18next = require("i18next");
+const Backend = require("i18next-node-fs-backend");
+const middleware = require("i18next-express-middleware");
+
+i18next
+  .use(Backend)
+  .use(middleware.LanguageDetector)
+  .init({
+    fallbackLng: "en", // Fallback language
+    backend: {
+      loadPath: __dirname + "/locales/{{lng}}/translation.json", //path to translation files
+    },
+  });
+
+// Use i18next middleware
+app.use(middleware.handle(i18next));
+
 app.use(
   cors({
-    origin:   'https://pawkeeper.netlify.app',
+    origin: "https://pawkeeper.netlify.app",
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true,
-    allowedHeaders: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+    allowedHeaders: ["Content-Type", "Authorization", "Origin", "Accept"],
     maxAge: 86400,
   })
 );
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://pawkeeper.netlify.app');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Origin, Accept');
-  res.header('Access-Control-Allow-Credentials', true);
+  res.header("Access-Control-Allow-Origin", "https://pawkeeper.netlify.app");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, Origin, Accept"
+  );
+  res.header("Access-Control-Allow-Credentials", true);
   next();
 });
 
 require("./config")(app);
 
-app.get('/debug-env', (req, res) => {
+app.get("/debug-env", (req, res) => {
   res.json({
     origin: process.env.ORIGIN,
-    port: process.env.PORT
+    port: process.env.PORT,
   });
 });
 
@@ -50,10 +71,10 @@ const reviewRoutes = require("./routes/review.routes");
 app.use("/reviews", reviewRoutes);
 
 app.use((err, req, res, next) => {
-  if (err.name === 'CORSError') {
-    res.status(403).json({ 
-      message: 'CORS error', 
-      error: err.message 
+  if (err.name === "CORSError") {
+    res.status(403).json({
+      message: "CORS error",
+      error: err.message,
     });
   } else {
     next(err);
